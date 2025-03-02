2c7661fbb05223ce2ea210a93df41e4aa8dc3a2a
Andrey Oliveira <dedey.cardoso@gmail.com>
2025-03-02 20:55:30 -0300

    Home page

--- src/app/components/icon-button.tsx
+++ src/app/components/icon-button.tsx
@@ -1,11 +1,13 @@
-import { ComponentProps, ReactNode } from "react"
+import { type ComponentProps, ReactNode } from 'react'
 
 interface IconButtonProps extends ComponentProps<'button'> {}
 
 export function IconButton(props: IconButtonProps) {
   return (
-    <button className="px-5 h-12 bg-gray-500 text-blue rounded-md 
+    <button
+      className="px-5 h-12 bg-gray-500 text-blue rounded-md 
     hover:bg-blue hover:text-gray-900 transition-colorsduration-200"
-    {...props}/>
+      {...props}
+    />
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,77 @@
-import { ArrowRight, Copy, Mail } from "lucide-react";
-import { Button } from "./components/button";
-import { IconButton } from "./components/icon-button";
-import { InputField, InputIcon, InputRoot } from "./components/input";
+import Image from 'next/image'
+
+import { ArrowRight, Mail, Radio, User } from 'lucide-react'
+import logo from '../assets/logo.svg'
+import { Button } from './components/button'
+import { InputField, InputIcon, InputRoot } from './components/input'
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-xl font-semibold">Hello World</h1>
-      <Button type="submit">
-        Enviar
-        <ArrowRight></ArrowRight>
-      </Button>
+    <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
+      <div className="min-h-dvh flex flex-col justify-center gap-16">
+        <div className="flex flex-col gap-8 items-center md:items-start w-full max-w-[550px]">
+          <Image src={logo} alt="devstage" width={108.5} height={30} />
 
-      <IconButton>
-        <Copy></Copy>
-      </IconButton>
+          <h1
+            className="text-4xl text-center leading-none font-heading font-medium 
+          flex flex-col md:text-7xl md:text-left"
+          >
+            <span className="text-blue">Codecraft</span>
+            Summit 2025
+          </h1>
+        </div>
 
-      <div>
-        <InputRoot>
-          <InputIcon>
-            <Mail />
-          </InputIcon>  
-          <InputField />
-        </InputRoot>
+        <div className="flex gap-5 items-stretch flex-col md:flex-row">
+          <div className="flex-1 bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6">
+            <div className="flex items-center justify-between">
+              <h2 className="font-heading font-semibold text-gray-200 text-xl">
+                Sobre o evento
+              </h2>
+              <span className="text-purple font-semibold text-xs flex items-center gap-2">
+                <Radio className="size-5"></Radio>
+                AO VIVO
+              </span>
+            </div>
+            <p className="text-gray-300 leading-relaxed text-sm md:text-base">
+              Um evento feito por e para pessoas desenvolvedoras apaixonadas por
+              criar soluções inovadoras e compartilhar conhecimento. Vamos
+              mergulhar nas tendências mais recentes em desenvolvimento de
+              software, arquitetura de sistemas e tecnologias emergentes, com
+              palestras, workshops e hackathons.
+            </p>
+          </div>
+          <form
+            className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6
+          w-full md:max-w-[440px]"
+            action=""
+          >
+            <h2 className="font-heading font-semibold text-gray-200 text-xl">
+              Inscrição
+            </h2>
+
+            <div className="space-y-3">
+              <InputRoot>
+                <InputIcon>
+                  <User />
+                </InputIcon>
+                <InputField type="text" placeholder="Nome completo" />
+              </InputRoot>
+
+              <InputRoot>
+                <InputIcon>
+                  <Mail />
+                </InputIcon>
+                <InputField type="email" placeholder="E-mail" />
+              </InputRoot>
+            </div>
+
+            <Button type="submit">
+              Confirmar
+              <ArrowRight />
+            </Button>
+          </form>
+        </div>
       </div>
-
     </main>
-  );
+  )
 }
